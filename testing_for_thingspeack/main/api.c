2e038f8d554ec23bdaf70c7c2ec608d01404f4db
Zizii2 <166628072+Azzure45@users.noreply.github.com>
2025-10-09 13:40:50 +0200

    not done

--- testing_for_thingspeack/main/api.c
+++ testing_for_thingspeack/main/api.c
@@ -54,4 +54,5 @@ void api_read(Api_handle data){
     printf("9. läser data från ThingSpeak...\n");
     // Skicka HTTP request
     esp_err_t http_err = esp_http_client_perform(data->client);
+    data->client->response
 }
